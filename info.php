6ad34825274e710f00f4945aa866123b5940755c
Dave Matheis <dmatheis@LSI.LOCAL>
2018-04-12 08:57:22 -0400

    Initial commit

---
+++ info.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmatheis
+ * Date: 4/11/2018
+ * Time: 8:48 PM
+ */
+phpinfo();
---
+++ tests/EmailTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmatheis
+ * Date: 4/11/2018
+ * Time: 8:54 PM
+ */
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
